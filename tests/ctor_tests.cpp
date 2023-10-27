266e2593aa0e1d1cd5aa0ddd5e84130424cbc6eb
Vicky <59438748+rkv-2401@users.noreply.github.com>
2023-10-27 19:19:58 +1100

    Update ctor_tests.cpp

--- tests/ctor_tests.cpp
+++ tests/ctor_tests.cpp
@@ -1,6 +1,6 @@
 //Single argument ctor:
 TEST_CASE("Constructor with single argument"){
-	auto const single_arg_ctr = comp6771::euclidean_vector(4);
+	auto const single_arg_ctr = ev::euclidean_vector(4);
 	auto single_arg_vector = std::vector<double>();
 	for (auto iter = 0; iter < 4; iter++){
 		single_arg_vector.push_back(0.0);
@@ -12,7 +12,7 @@ TEST_CASE("Constructor with single argument"){
 
 //Two argument ctor:
 TEST_CASE("Constructor with two arguments"){
-	auto const double_arg_ctr = comp6771::euclidean_vector(4, 2.0);
+	auto const double_arg_ctr = ev::euclidean_vector(4, 2.0);
 	auto double_arg_vector = std::vector<double>();
 	for (auto iter = 0; iter < 4; iter++){
 		double_arg_vector.push_back(2.0);
@@ -29,7 +29,7 @@ TEST_CASE("Constructor with iterator"){
 	iter_vec.push_back(0.0);
 	iter_vec.push_back(5.0);
 	iter_vec.push_back(55.0);
-	auto const iter_arg_ctr = comp6771::euclidean_vector(iter_vec.begin(), iter_vec.end());
+	auto const iter_arg_ctr = ev::euclidean_vector(iter_vec.begin(), iter_vec.end());
 	CHECK(iter_arg_ctr.dimensions() == 4);
 	CHECK(iter_arg_ctr.get_mag() == iter_vec);
 
@@ -39,7 +39,7 @@ TEST_CASE("Constructor with iterator"){
 TEST_CASE("Constructor with Initializer List"){
 	auto init_list = {2.0, 0.0, 5.0, 6.2, 7.0, 8.4};
 	auto init_vector = std::vector<double>(init_list);
-	auto const init_list_ctr = comp6771::euclidean_vector(init_list);
+	auto const init_list_ctr = ev::euclidean_vector(init_list);
 	CHECK(init_list_ctr.dimensions() == 6);
 	CHECK(init_list_ctr.get_mag() == init_vector);
 
@@ -47,8 +47,8 @@ TEST_CASE("Constructor with Initializer List"){
 
 //Copy ctor:
 TEST_CASE("Copy constructor test"){
-	auto const double_arg_ctr = comp6771::euclidean_vector(4, 2.0);
-	auto copy_ctr = comp6771::euclidean_vector(double_arg_ctr);
+	auto const double_arg_ctr = ev::euclidean_vector(4, 2.0);
+	auto copy_ctr = ev::euclidean_vector(double_arg_ctr);
 	auto double_arg_vector_cpy = std::vector<double>();
 	for (auto iter = 0; iter < 4; iter++){
 		double_arg_vector_cpy.push_back(2.0);
@@ -59,9 +59,9 @@ TEST_CASE("Copy constructor test"){
 
 //Move ctor:
 TEST_CASE("Move constructor test"){
-	auto const double_arg_ctr = comp6771::euclidean_vector(4, 2.0);
-	auto temp_move_ctr = comp6771::euclidean_vector(double_arg_ctr);
-	auto move_ctr = comp6771::euclidean_vector(std::move(temp_move_ctr));
+	auto const double_arg_ctr = ev::euclidean_vector(4, 2.0);
+	auto temp_move_ctr = ev::euclidean_vector(double_arg_ctr);
+	auto move_ctr = ev::euclidean_vector(std::move(temp_move_ctr));
 	auto double_arg_vector_cpy = std::vector<double>();
 	for (auto iter = 0; iter < 4; iter++){
 		double_arg_vector_cpy.push_back(2.0);
